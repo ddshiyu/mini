32220e8256a3cfbae0934d150ab0f43261b8e9f2
东东 <925843934@qq.com>
2020-11-23 23:34:23 +0800

    weui

--- pages/account/account.js
+++ pages/account/account.js
@@ -1,65 +1,36 @@
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    
+      iosDialog1: false,
+      iosDialog2: false,
+      androidDialog1: false,
+      androidDialog2: false,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  close: function () {
+      this.setData({
+          iosDialog1: false,
+          iosDialog2: false,
+          androidDialog1: false,
+          androidDialog2: false,
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  openIOS1: function() {
+      this.setData({
+          iosDialog1: true
+      });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
+  openIOS2: function() {
+      this.setData({
+          iosDialog2: true
+      });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
+  openAndroid1: function() {
+      this.setData({
+          androidDialog1: true
+      });
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
+  openAndroid2: function() {
+      this.setData({
+          androidDialog2: true
+      });
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
-})
+});
---
+++ pages/actionsheet/actionsheet.js
@@ -0,0 +1,22 @@
+Page({
+    data: {
+        showIOSDialog: false,
+        showAndroidDialog: false,
+    },
+    close: function() {
+        this.setData({
+            showIOSDialog: false,
+            showAndroidDialog: false
+        });
+    },
+    openIOS: function () {
+        this.setData({
+            showIOSDialog: true
+        });
+    },
+    openAndroid: function () {
+        this.setData({
+            showAndroidDialog: true
+        });
+    }
+});
